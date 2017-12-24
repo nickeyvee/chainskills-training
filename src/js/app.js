8f525742457048f35af59d42c444b516d6b892cd
Nicholas  Vorraso <nvorraso2020@gmail.com>
2017-12-23 17:51:12 -0800

    Created functions for initalizing the app.

--- src/js/app.js
+++ src/js/app.js
@@ -1,55 +1,55 @@
 App = {
   web3Provider: null,
   contracts: {},
+  account: 0x0,
 
   init: function() {
-    /*
-     * Replace me...
-     */
-
     return App.initWeb3();
   },
 
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
 
+    if(typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+      web3 = new Web3(App.web3Provider);
+    }
+
+    App.displayAccountInfo();
     return App.initContract();
   },
 
+  displayAccountInfo: function() {
+    web3.eth.getCoinbase(( err, account ) => {
+      if(err === null) {
+        console.log("no error");
+        App.account = account;
+        $('#account').text(account);
+        web3.eth.getBalance(account, (err, balance) => {
+          if(err === null) {
+            $('#accountBalance').text(web3.fromWei(balance, 'ether') + "ETH");
+          }
+        })
+      } else {
+        console.log(err);
+      }
+    })
+  },
+
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON('Chainlist.json', chainListArtifact => {
+      App.contracts.Chainlist = TruffleContract(chainListArtifact);
 
-    return App.bindEvents();
-  },
-
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
-
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
-
-  handleAdopt: function(event) {
-    event.preventDefault();
-
-    var petId = parseInt($(event.target).data('id'));
-
-    /*
-     * Replace me...
-     */
+      App.contracts.Chainlist.setProvider(App.web3Provider);
+    })
   }
-
 };
 
 $(function() {
   $(window).load(function() {
+    console.log("app init..")
     App.init();
   });
 });
